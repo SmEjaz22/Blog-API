6bdf7a770d9b82422569b2d5898444a6886c1745
SM Ejaz Hasnain <s.mejazhasnain22@gmail.com>
2024-10-13 23:44:34 +0500

    app 'accounts' with register/signup creation

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
 
 app_name='accounts'
+from . import views
 
 urlpatterns = [
     path('',include('django.contrib.auth.urls')),
+    path('register/',views.register,name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
 # Django uses a default view function for login template. We don't need to create one.
+# We are using Django default USERCREATIONFORM but write our own views and templates.
+
+def register(request):
+    """Registering a new user """
+    if request.method != 'POST':
+        form=UserCreationForm()
+    else:
+         form=UserCreationForm(data=request.post)
+    if form.is_valid():
+        newuser=form.save()
+        login(request,newuser) # As we are seeing here, login takes newuser in it's parame.
+        return redirect('blogs:all_blogs')
+    context={'form':form}
+    return render(request,'registration/register.html',context)     
+    
