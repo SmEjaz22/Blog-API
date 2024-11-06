f7374a5962067a11c536e8ff41576ca498106136
SM Ejaz Hasnain <s.mejazhasnain22@gmail.com>
2024-11-06 23:37:42 +0500

    Browsable API Created
    Backend is now capable of communicating with any FrontEnd

--- Blog/settings.py
+++ Blog/settings.py
@@ -56,11 +56,15 @@ INSTALLED_APPS = [
     
     #API's
     'rest_framework',
+    'corsheaders' #Cross-Origin Resource Sharing
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    #cors
+    "corsheaders.middleware.CorsMiddleware",
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -70,6 +74,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+
+CORS_ALLOWED_ORIGINS = ( 
+"http://localhost:8080", 
+"http://localhost:8000", 
+)
+
+
 
 ROOT_URLCONF = 'Blog.urls'
 
@@ -183,3 +196,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ORIGIN_WHITELIST = ( 
+"http://localhost:8080", 
+"http://localhost:8000", 
+)
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"] 
